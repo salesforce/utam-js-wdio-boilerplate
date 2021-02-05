70361c4d3a03c64b7438d49651ca1c510ba076f5
Diego Ferreiro Val <dval@salesforce.com>
2021-02-04 18:20:37 -0800

    chore: minor fixes

--- src/__tests__/__wdio__/example.spec.ts
+++ src/__tests__/__wdio__/example.spec.ts
@@ -1,10 +1,12 @@
+// Import a PageObject from within the package
 import LwcDevHomePage from 'utam-js-webdriverio-boilerplate/pageObjects/lwc-home';
+
 describe('UTAM WDIO example', () => {
     it('has correct header - lwc.dev', async () => {
         const expectedHeader = 'Lightning Web Components';
 
         // Load the website
-        await browser.url('http://lwc.dev');
+        await browser.url('https://lwc.dev');
 
         // Load the root Utam Page Object
         const lwcDevHomePage = await utam.load(LwcDevHomePage);
