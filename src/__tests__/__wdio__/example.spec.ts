a8b76dda15219bca61bcbb8785e3ca400e5f6f52
Diego Ferreiro Val <dval@salesforce.com>
2021-02-04 18:17:15 -0800

    chore: add comments

--- src/__tests__/__wdio__/example.spec.ts
+++ src/__tests__/__wdio__/example.spec.ts
@@ -1,9 +1,19 @@
 import LwcDevHomePage from 'utam-js-webdriverio-boilerplate/pageObjects/lwc-home';
-describe('counter specs test', () => {
-    it('has header - lwc.dev', async () => {
+describe('UTAM WDIO example', () => {
+    it('has correct header - lwc.dev', async () => {
+        const expectedHeader = 'Lightning Web Components';
+
+        // Load the website
         await browser.url('http://lwc.dev');
+
+        // Load the root Utam Page Object
         const lwcDevHomePage = await utam.load(LwcDevHomePage);
+        
+        // Check for all the methods (it should autocomplete for you)
         const header = await lwcDevHomePage.getHeader();
-        expect(await header.getText()).toBe('Lightning Web Components');
+        const headerText = await header.getText();
+
+        // Asser that the header title text is what we expect
+        expect(headerText).toBe(expectedHeader);
     });
 });
